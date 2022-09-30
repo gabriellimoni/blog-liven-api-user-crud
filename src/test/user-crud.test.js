78a2f0ab7e38d4204927689f1bc2a1dee8cc8359
Gabriel Limoni <gabrielblimoni@gmail.com>
2022-09-30 15:48:11 -0300

    refactor: better test structure

--- src/test/user-crud.test.js
+++ src/test/user-crud.test.js
@@ -3,49 +3,63 @@ import request from "supertest";
 import server from "../server.js";
 import UserModel from "../models/user.js";
 
-describe("Create user endpoint", () => {
+describe("User resource", () => {
   beforeAll(async () => {
     await connect();
-  });
-  beforeEach(async () => {
-    await UserModel.deleteMany({});
   });
   afterAll(async () => {
     await disconnect();
   });
 
-  test("Should create user successfully", async () => {
-    const response = await request(server)
-      .post("/user")
-      .send({
+  describe("Create user endpoint", () => {
+    beforeEach(async () => {
+      await UserModel.deleteMany({});
+    });
+
+    test("Should create user successfully", async () => {
+      const response = await request(server)
+        .post("/user")
+        .send({
+          name: "Any name",
+          email: "any@mail.com",
+        })
+        .set("Content-Type", "application/json");
+
+      const userFromDatabase = await UserModel.findOne({
+        id: response.body.id,
+      });
+
+      expect(response.statusCode).toBe(200);
+      expect(response.body).toMatchObject({
+        id: expect.any(String),
         name: "Any name",
         email: "any@mail.com",
-      })
-      .set("Content-Type", "application/json");
-
-    const userFromDatabase = await UserModel.findOne({ id: response.body.id });
-
-    expect(response.statusCode).toBe(200);
-    expect(response.body).toMatchObject({
-      id: expect.any(String),
-      name: "Any name",
-      email: "any@mail.com",
+      });
+      expect(response.body).toMatchObject({
+        id: userFromDatabase.id,
+        name: userFromDatabase.name,
+        email: userFromDatabase.email,
+      });
     });
-    expect(response.body).toMatchObject({
-      id: userFromDatabase.id,
-      name: userFromDatabase.name,
-      email: userFromDatabase.email,
+    test("Should return error if email not sent", async () => {
+      const response = await request(server)
+        .post("/user")
+        .send({
+          name: "Any name",
+        })
+        .set("Content-Type", "application/json");
+
+      expect(response.statusCode).toBe(400);
     });
-  });
+    test("Should return error if name not sent", async () => {
+      const response = await request(server)
+        .post("/user")
+        .send({
+          email: "any@mail.com",
+        })
+        .set("Content-Type", "application/json");
 
-  test("Should return error if email not sent", async () => {
-    const response = await request(server)
-      .post("/user")
-      .send({
-        name: "Any name",
-      })
-      .set("Content-Type", "application/json");
-
-    expect(response.statusCode).toBe(400);
+      expect(response.statusCode).toBe(400);
+    });
   });
 });
